76d98398c29b7d90a72eb676bee6b3bdd9526b2d
shiqstone <shiqstone@gmail.com>
2024-03-25 12:14:25 +0800

    update api response model

--- lib/model/deposit_conf_resp.dart
+++ lib/model/deposit_conf_resp.dart
@@ -2,14 +2,16 @@ class DepositConfResp {
   late int statusCode;
   String? statusMsg;
   late List<Map<String, dynamic>> products;
+  late List<Map<String, dynamic>> payways;
   String? tips;
 
-  DepositConfResp(this.statusCode, this.statusMsg, this.products, this.tips);
+  DepositConfResp(this.statusCode, this.statusMsg, this.products, this.payways, this.tips);
 
   DepositConfResp.fromJson(Map<String, dynamic> json) {
     statusCode = json['status_code'];
     statusMsg = json['status_msg'];
     products = json['products'].cast<Map<String, dynamic>>();
+    payways = json['payways'].cast<Map<String, dynamic>>();
     tips = json['tips'];
   }
 
@@ -17,6 +19,7 @@ class DepositConfResp {
         'status_code': statusCode,
         'status_msg': statusMsg,
         'products': products,
+        'payways': payways,
         'tips': tips,
       };
 }
